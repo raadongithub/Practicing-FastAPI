931fab8576d995271b77e5484284ab460b95e5c6
Raad <raadonmail@gmail.com>
2025-07-12 11:04:07 +0500

    Path and Query Funcs

--- 1Get/get.py
+++ 1Get/get.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
+
 import json 
 
 
@@ -17,7 +18,28 @@ def about():
     return {"message": "Kya karu apni tareef mai?"}
 
 
-@app.get("/view")
-def view():
+@app.get("/view/{patient_id}")
+def view(patient_id:str=Path(..., description="Patient ID to view details", example='P001')):
     data = load_data()
-    return data
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Ye bnda abi mareez nai bna")
+
+
+@app.get("/sort/")
+def sort_patients(sort_by:str=Query(..., description='Height, Weight, BMI'), order:str=Query('asc', description="'asc'ending order or 'desc'ending order")):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        HTTPException(status_code=400, detail='Sahi Field Choose kr ustad')
+
+    if order not in ['asc', 'dsc']:
+        HTTPException(status_code=400, detail='Sahi order Choose kr ustaddd')
+
+    data = load_data()
+    sort_order = True if order=='desc' else False
+    sorted_date = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+    return sorted_date
+    
